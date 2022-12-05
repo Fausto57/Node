a42ba07aabc6b491169dbb8b3a305d50c03404cb
fausto57 <Faustomzp@gmail.com>
2022-12-04 21:47:04 -0700

    Agregando cors

--- app.js
+++ app.js
@@ -3,3 +3,13 @@ const express = require('express')
 const app = express()
 
 const start = require('./system/core/start')(app);
+
+const cors = require('cors');
+
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+
+app.use(cors(corsOptions));
