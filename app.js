49c0900465eddabb6628664fa233c77b0419cce6
fausto57 <Faustomzp@gmail.com>
2022-12-05 13:10:38 -0700

    cambios al momento

--- app.js
+++ app.js
@@ -4,4 +4,12 @@ const app = express()
 
 const start = require('./system/core/start')(app);
 
+const cors = require('cors');
 
+const corsOptions ={
+    origin:'http://localhost:8080', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+
+app.use(cors(corsOptions));
