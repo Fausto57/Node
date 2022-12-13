cc4bd92e3534634c853cc9af23c61438a41c214f
fausto57 <Faustomzp@gmail.com>
2022-12-12 23:50:52 -0700

    Arreglando cors

--- system/core/start.js
+++ system/core/start.js
@@ -19,13 +19,29 @@ function InitApp(app){
     const port = process.env.PORT || 3000;
     app.use(bodyParser.json())
 
+    
+
+app.use((req, res, next) => {
+    //allow access from every, elminate CORS
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.removeHeader('x-powered-by');
+    //set the allowed HTTP methods to be requested
+    res.setHeader('Access-Control-Allow-Methods','POST');
+    //headers clients can use in their requests
+    res.setHeader('Access-Control-Allow-Headers','Content-Type');
+    //allow request to continue and be handled by routes
+    next();
+  });
+/*
     app.use(cors({
         origin: '*',
         methods: '*',
         optionsSuccessStatus: 200,
-        'Access-Control-Allow-Origin': '*'
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Credentials': true
 
-    }));    
+    }));  */ 
 
     app.listen(port, () => console.log(`Corriendo en el puerto ${port}`))
 }
